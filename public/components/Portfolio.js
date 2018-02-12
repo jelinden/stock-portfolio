9113642e0bf965926bec0dcb586b08f962e1167a
Jouni Linden <jouni.linden@gmail.com>
2018-02-12 18:50:56 +0200

    check for undefined in numberFormat

--- public/components/Portfolio.js
+++ public/components/Portfolio.js
@@ -59,8 +59,11 @@ class Portfolio extends React.Component {
   }
 
   numberFormat(n) {
-    var parts = n.toString().split(".");
-    return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ") + (parts[1] ? "." + parts[1] : "");
+    if (n !== undefined) {
+      var parts = n.toString().split(".");
+      return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, " ") + (parts[1] ? "." + parts[1] : "");
+    }
+    return '';
   }
 
   componentDidMount() {
