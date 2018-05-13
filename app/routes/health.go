062a0c310c25705792856c1e769c4de27527fc22
Jouni Linden <jouni.linden@gmail.com>
2018-05-13 11:10:10 +0300

    program memory allocation to health api

--- app/routes/health.go
+++ app/routes/health.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"runtime"
 	"time"
 
 	"github.com/jelinden/stock-portfolio/app/util"
@@ -14,9 +15,10 @@ import (
 )
 
 type SystemHealth struct {
-	MemUsedPercent []float64
-	CPUTotal       float64
-	DiskUsage      float64
+	MemUsedPercent  []float64
+	ProgramMemUsage []uint64
+	CPUTotal        float64
+	DiskUsage       float64
 }
 
 var health SystemHealth
@@ -24,6 +26,7 @@ var health SystemHealth
 func init() {
 	go util.DoEvery(time.Second*5, getHealth)
 	go util.DoEvery(time.Minute, getMemory)
+	go util.DoEvery(time.Minute, programMemUsage)
 }
 
 func Health(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
@@ -47,11 +50,29 @@ func getHealth() {
 func getMemory() {
 	v, _ := mem.VirtualMemory()
 	mem := health.MemUsedPercent
-	if len(mem) == 61 {
+	if len(mem) == 60 {
 		copy := mem[1:]
 		copy = append(copy, v.UsedPercent)
 		health.MemUsedPercent = copy
 	} else {
 		health.MemUsedPercent = append(mem, v.UsedPercent)
 	}
+}
+
+func programMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	alloc := bToMb(m.Alloc)
+	programMem := health.ProgramMemUsage
+	if len(programMem) == 60 {
+		copy := programMem[1:]
+		copy = append(copy, alloc)
+		health.ProgramMemUsage = copy
+	} else {
+		health.ProgramMemUsage = append(programMem, alloc)
+	}
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
 }
