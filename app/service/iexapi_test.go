0139ce71cc98b8f0bbd7e20b266475ba49b6053c
Jouni Linden <jouni.linden@gmail.com>
2018-06-03 14:53:39 +0300

    tests for iexapi fetches

--- app/service/iexapi_test.go
+++ app/service/iexapi_test.go
@@ -1,10 +1,22 @@
 package service
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetQuotes(t *testing.T) {
-	fmt.Println(GetQuotes("ADP", "OHI", "O", "XOM"))
+	quotes := GetQuotes("XOM")
+	assert.True(t, quotes[0].Symbol == "XOM", "symbol should be XOM")
+}
+
+func TestGetDividends(t *testing.T) {
+	dividends := GetDividends("XOM")
+	assert.True(t, dividends[0].Symbol == "XOM", "symbol should be XOM")
+}
+
+func TestGetClosePrices(t *testing.T) {
+	closePrices := GetClosePrices("ADP")
+	assert.True(t, closePrices[0].Symbol == "ADP", "symbol should be ADP")
 }
