d7abf1a7976f52a0ba90cdd29ccae768ed1feba2
Jouni Linden <jouni.linden@gmail.com>
2022-03-02 18:24:04 +0200

    get dividends twice a week

--- app/service/iexapi_test.go
+++ app/service/iexapi_test.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -20,6 +21,19 @@ func TestGetDividends(t *testing.T) {
 	config.Config.Token = os.Getenv("IEXAPITOKEN")
 	dividends := GetDividends("XOM")
 	assert.True(t, dividends[0].Symbol == "XOM", "symbol should be XOM")
+}
+
+func TestGetDividendsTRI(t *testing.T) {
+	config.Config.FromEmail = os.Getenv("FROMEMAIL")
+	config.Config.EmailSendingPasswd = os.Getenv("EMAILSENDINGPASSWD")
+	config.Config.AdminUser = os.Getenv("ADMINUSER")
+	config.Config.Token = os.Getenv("IEXAPITOKEN")
+	dividends := GetDividends("TRI")
+	assert.True(t, dividends[0].Symbol == "TRI", "symbol should be TRI")
+	fmt.Println(dividends[0])
+	assert.True(t, dividends[0].Amount >= 0.571247)
+	assert.True(t, dividends[0].Currency == "CAD")
+	assert.True(t, dividends[0].Symbol == "TRI")
 }
 
 func TestGetClosePrices(t *testing.T) {
